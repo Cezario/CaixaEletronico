a42187a04596a4de61bbae8bff750036c6a2f095
karlostiago <carlostiagodesousa@gmail.com>
2021-08-07 12:15:53 -0300

    Refatoracao

---
+++ src/main/java/com/bancojompa/caixaeletronico/BancojompaCaixaeletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.bancojompa.caixaeletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancojompaCaixaeletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancojompaCaixaeletronicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancojompa/caixaeletronico/BancojompaCaixaeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancojompa.caixaeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancojompaCaixaeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
